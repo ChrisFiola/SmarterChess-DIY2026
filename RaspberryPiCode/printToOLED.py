33d866d3898beeae53e6872f3f82f235455d73a5
Christopher Fiola <36894544+ChrisFiola@users.noreply.github.com>
2026-01-19 17:14:44 -0500

    Update printToOLED.py

--- RaspberryPiCode/printToOLED.py
+++ RaspberryPiCode/printToOLED.py
@@ -6,22 +6,23 @@ argv = sys.argv[1:]
 textLine1 = ''
 textLine2 = ''
 textLine3 = ''
+textLine4 = ''
 textSize = ''
 
 # Work through those arguments
 try:
     opts, args = getopt.getopt(
         argv,
-        "ha:b:c:s:",
-        ["firstLine=", "secondLine=", "thirdLine=", "textSize="]
+        "ha:b:c:d:s:",
+        ["firstLine=", "secondLine=", "thirdLine=", "fourthLine=", "textSize="]
     )
 except getopt.GetoptError:
-    print('printToConsole.py -a <firstline> -b <secondline> -c <thirdline> -s <textsize>')
+    print('printToConsole.py -a <firstline> -b <secondline> -c <thirdline> -d <fourthline> -s <textsize>')
     sys.exit(2)
 
 for opt, arg in opts:
     if opt == '-h':
-        print('printToConsole.py -a <firstline> -b <secondline> -c <thirdline> -s <textsize>')
+        print('printToConsole.py -a <firstline> -b <secondline> -c <thirdline> -d <fourthline> -s <textsize>')
         sys.exit()
     elif opt in ("-a", "--firstLine"):
         textLine1 = arg
@@ -29,6 +30,8 @@ for opt, arg in opts:
         textLine2 = arg
     elif opt in ("-c", "--thirdLine"):
         textLine3 = arg
+    elif opt in ("-d", "--fourthLine"):
+        textLine4 = arg
     elif opt in ("-s", "--textSize"):
         textSize = int(arg)
 
@@ -38,4 +41,5 @@ print(f"Text Size: {textSize}")
 print(f"{textLine1.center(40)}")
 print(f"{textLine2.center(40)}")
 print(f"{textLine3.center(40)}")
+print(f"{textLine4.center(40)}")
 print("="*40 + "\n")
