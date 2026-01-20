61783018fbd2b6c9de3d1620fc2ccbb605ebea88
Christopher Fiola <36894544+ChrisFiola@users.noreply.github.com>
2026-01-20 15:26:31 -0500

    Update smartchess.py

--- SmarterChess/smartchess.py
+++ SmarterChess/smartchess.py
@@ -95,7 +95,7 @@ def restart_display_server():
 
 
 def send_to_screen(
-    line1: str, line2: str = "", line3: str = "", line4: str = "", size: int = ""
+    line1: str, line2: str = "", line3: str = "", line4: str = "", size: int = 0
 ) -> None:
     """Fire-and-forget update to OLED (non-blocking)."""
     try:
