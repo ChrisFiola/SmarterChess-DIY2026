7911a43a55081c7f6573d1d78939a42de2488569
Christopher Fiola <36894544+ChrisFiola@users.noreply.github.com>
2026-01-20 15:27:29 -0500

    Update printToOLED.py

--- RaspberryPiCode/printToOLED.py
+++ RaspberryPiCode/printToOLED.py
@@ -31,7 +31,7 @@ for opt, arg in opts:
 # Auto-size text if not manually given
 line_count = len([t for t in [text1, text2, text3, text4] if t])
 
-if textSize is None:
+if textSize is 0:
     if line_count == 1:
         textSize = 48
     elif line_count == 2:
