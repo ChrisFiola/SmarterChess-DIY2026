61d683ba361bc3b2bbb96a49cdd469bdf3874287
Chris Fiola <christopherfiola@gmail.com>
2026-01-27 13:58:49 -0500

    trying to fix game over board

--- PicoCode/main/main.py
+++ PicoCode/main/main.py
@@ -870,7 +870,6 @@ def game_over_wait_ok_and_ack(result_str):
     buttons.reset()
     cp.coord(False); cp.hint(True); cp.ok(True)
     board.show_checkmate_scene_hash()
-    time.sleep_ms(500)
 
     # Optional: blink OK pixel while waiting
     blink = False
--- RaspberryPiCode/main/piGame.py
+++ RaspberryPiCode/main/piGame.py
@@ -240,10 +240,13 @@ def handoff_next_turn(link: BoardLink, display: Display, brd: chess.Board, mode:
 
 def engine_move_and_send(link: BoardLink, display: Display, ctx: EngineContext, state: RuntimeState, cfg: GameConfig):
     reply = engine_bestmove(ctx, state.board, cfg.move_time_ms)
+    
     if reply is None:
         return
-    state.board.push_uci(reply)
-    link.sendtoboard(f"m{reply}")
+    
+    if not state.board.is_game_over():
+        state.board.push_uci(reply)
+        link.sendtoboard(f"m{reply}")
 
     if state.board.is_game_over():
         _res = report_game_over(link, display, state.board)
