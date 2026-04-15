40564d18de80193bca46af6f324de18f03852f10
Chris Fiola <christopherfiola@gmail.com>
2026-04-15 10:29:11 -0400

    revert changes

--- RaspberryPiCode/piMain.py
+++ RaspberryPiCode/piMain.py
@@ -41,7 +41,6 @@ def main():
     cfg = GameConfig()
     state = GameState(board=chess.Board(), mode="stockfish")
     link.set_touch_queue(display.touch_queue)
-    link.send_to_board("heyArduinoReady")
 
     try:
         while True:
